ad1e2cf1abcaa8c9db2a2ace19d8b1cb8b5421f4
Balazs Mester <bmester@cloudera.com>
2021-03-01 08:34:31 +0100

    Add routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import MainPage from "./components/MainPage.vue";
+import SavedTables from "./components/SavedTables.vue";
 import { store } from './store/store.js';
+import VueRouter from 'vue-router';
 
+
+const router = new VueRouter({
+  routes: [
+    { path: "", component: MainPage },
+    { path: "/tables", component: SavedTables }
+  ]
+});
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.directive("uppercase", {
   update: function(el) {
@@ -12,4 +24,5 @@ Vue.directive("uppercase", {
 new Vue({
   store: store,
   render: (h) => h(App),
+  router
 }).$mount("#app");
